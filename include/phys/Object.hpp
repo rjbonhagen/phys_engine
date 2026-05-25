39dc729225d3807fd4421db3a6926126db81e3e3
RJ Bonhagen <rjbonhagen02@gmail.com>
2026-05-25 01:15:49 -0700

    add Object ==

--- include/phys/Object.hpp
+++ include/phys/Object.hpp
@@ -16,6 +16,11 @@ namespace phys
 
         Object(Vec2 p = {0,0}, Vec2 v = {0,0}, Vec2 a = {0,0}, Vec2 f = {0,0}, real m = 0) : position(p), velocity(v), acceleration(a), forces(f), mass(m) {};
         virtual ~Object() = default;
+        bool operator==(const Object& o) const { return (position == o.position) && 
+                                                        (velocity == o.velocity) && 
+                                                        (acceleration == o.acceleration) && 
+                                                        (forces == o.forces) && 
+                                                        (mass == o.mass); };
     };
 
 
