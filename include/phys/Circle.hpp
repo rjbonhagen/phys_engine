987f671110c2d6a09c9187070fa14ddbb1778593
RJ Bonhagen <rjbonhagen02@gmail.com>
2026-05-29 00:16:27 -0700

    remove Circle defautl constructor

--- include/phys/Circle.hpp
+++ include/phys/Circle.hpp
@@ -7,7 +7,7 @@ namespace phys
     {
         real radius;
 
-        Circle(Vec2 position = {}, Vec2 velocity = {}, Vec2 acceleration = {}, Vec2 forces = {}, real mass = 0.0f, real restitution = 0.5f, real radius = 0.0f)
+        Circle(Vec2 position, Vec2 velocity, Vec2 acceleration, Vec2 forces, real mass, real restitution = 0.5f, real radius)
             : Object(position, velocity, acceleration, forces, mass, restitution), radius(radius) {}
 
     };
