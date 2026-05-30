f46b85d0dea5496f8ad60e2882de6fdc8101e857
RJ Bonhagen <rjbonhagen02@gmail.com>
2026-05-30 00:36:51 -0700

    cleaner

--- include/phys/AABB.hpp
+++ include/phys/AABB.hpp
@@ -4,13 +4,12 @@
 
 namespace phys
 {
-    struct AABB : public Object
+    struct AABB : Object
     {
-    public:
         Vec2 min;
         Vec2 max;
 
-        AABB(Vec2 min, Vec2 max, Vec2 velocity, Vec2 acceleration, Vec2 forces, real mass, real restitution) 
+        AABB(Vec2 min, Vec2 max, Vec2 velocity, Vec2 acceleration, Vec2 forces, real mass, real restitution)
         : min(min), max(max), Object(min, velocity, acceleration, forces, mass, restitution)
          { if (min == max) throw std::invalid_argument("min must not equal max"); }
 
