d17609c584f8733f4ee2dd96e55c82b007a67a1a
RJ Bonhagen <rjbonhagen02@gmail.com>
2026-06-01 19:54:19 -0700

    add min max

--- include/phys/AABB.hpp
+++ include/phys/AABB.hpp
@@ -13,11 +13,16 @@ namespace phys
         
         public:
 
+<<<<<<< HEAD
         AABB(Vec2 min, Vec2 max, Vec2 velocity, 
             Vec2 acceleration, Vec2 forces, 
             real mass, real restitution)
             : min(min), max(max), half_body((max - min) / 2), 
         Object((min + max) / 2, velocity, acceleration, forces, mass, restitution)
+=======
+        AABB(Vec2 min, Vec2 max, Vec2 velocity, Vec2 acceleration, Vec2 forces, real mass, real restitution)
+        : min(min), max(max), Object((min + max)/ 2, velocity, acceleration, forces, mass, restitution)
+>>>>>>> 796c363 (add min max)
          { if (min == max) throw std::invalid_argument("min must not equal max"); }
 
         Vec2 get_min() const { return position - half_body; }
