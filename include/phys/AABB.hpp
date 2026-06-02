64336069bc5642b562f6fa7a04c589204ff5b0e6
rjbonhagen <rjbonhagen02@gmail.com>
2026-06-01 19:51:49 -0700

    addd AABB halfbody

--- include/phys/AABB.hpp
+++ include/phys/AABB.hpp
@@ -6,12 +6,22 @@ namespace phys
 {
     struct AABB : Object
     {
+        private:
         Vec2 min;
         Vec2 max;
+        Vec2 half_body;
+        
+        public:
 
-        AABB(Vec2 min, Vec2 max, Vec2 velocity, Vec2 acceleration, Vec2 forces, real mass, real restitution)
-        : min(min), max(max), Object(min, velocity, acceleration, forces, mass, restitution)
+        AABB(Vec2 min, Vec2 max, Vec2 velocity, 
+            Vec2 acceleration, Vec2 forces, 
+            real mass, real restitution)
+            : min(min), max(max), half_body((max - min) / 2), 
+        Object((min + max) / 2, velocity, acceleration, forces, mass, restitution)
          { if (min == max) throw std::invalid_argument("min must not equal max"); }
 
+        Vec2 get_min() const { return position - half_body; }
+        Vec2 get_max() const { return position + half_body; }
+        Vec2 get_half_body() const { return half_body; }
     };
 }
